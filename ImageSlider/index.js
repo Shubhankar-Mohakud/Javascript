ef3c43f4d03dbf988bc2302c3cd9964b06c14ffe
Shubhankar-Mohakud <shubhankar.mohakud@outlook.com>
2026-07-12 20:21:32 +0530

    Image Slider

---
+++ ImageSlider/index.js
@@ -0,0 +1,36 @@
+// IMG SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex=0;
+let intervalId = null;
+
+// initializeSlider();
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId=setInterval(nextSlide, 5000);
+    }
+    
+}
+
+function showSlide(index) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[index].classList.add("displaySlide");
+}
+
+function prevSlide() {
+    clearInterval(intervalId);
+    slideIndex = (slideIndex-1 + slides.length)%slides.length;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex = (slideIndex+1) % slides.length;
+    showSlide(slideIndex);
+}
+
